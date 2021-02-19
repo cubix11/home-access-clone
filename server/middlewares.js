8cf3e3d7859e73a9686751d3a6e5af6e62ee15e1
loveCoding01 <ishan.karmakar24@gmail.com>
2021-02-19 10:58:20 -0600

    Function for sending email in middlewares.ts

--- server/middlewares.js
+++ server/middlewares.js
@@ -1,9 +1,30 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkUser = void 0;
+exports.sendEmail = exports.checkUser = void 0;
+const mail_1 = __importDefault(require("@sendgrid/mail"));
+const dotenv_1 = __importDefault(require("./dotenv"));
+const API_KEY = dotenv_1.default.SENDGRID_KEY;
+mail_1.default.setApiKey(API_KEY);
 function checkUser(req, res, next) {
     const bearerToken = req.get('Authorization');
     console.log(bearerToken);
     next();
 }
 exports.checkUser = checkUser;
+async function sendEmail(recieveEmail, subject, body) {
+    const email = {
+        to: recieveEmail,
+        from: {
+            email: 'karmakarfamily216php@gmail.com',
+            name: 'Home Access Mail'
+        },
+        subject,
+        text: body,
+        html: body
+    };
+    mail_1.default.send(email);
+}
+exports.sendEmail = sendEmail;
--- server/middlewares.ts
+++ server/middlewares.ts
@@ -1,7 +1,37 @@
 import { Request, Response, NextFunction } from 'express';
+import sendgrid from '@sendgrid/mail';
+import env from './dotenv';
+
+const API_KEY: string = env.SENDGRID_KEY;
+sendgrid.setApiKey(API_KEY);
+interface From {
+    email: string;
+    name: string;
+}
+interface Email {
+    to: string;
+    from: From;
+    subject: string;
+    text: string;
+    html: string;
+}
 
 export function checkUser(req: Request, res: Response, next: NextFunction): void {
-    const bearerToken = req.get('Authorization');
+    const bearerToken: string = req.get('Authorization');
     console.log(bearerToken);
     next();
+}
+
+export async function sendEmail(recieveEmail: string, subject: string, body: string): void {
+    const email: Email = {
+        to: recieveEmail,
+        from: {
+            email: 'karmakarfamily216php@gmail.com',
+            name: 'Home Access Mail'
+        },
+        subject,
+        text: body,
+        html: body
+    };
+    sendgrid.send(email);
 }
