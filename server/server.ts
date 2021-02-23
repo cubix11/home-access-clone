78d97b7428b872d7317fab16e210b11ce023081e
loveCoding01 <ishan.karmakar24@gmail.com>
2021-02-22 21:37:11 -0600

    Test

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import helmet from 'helmet'; // Import helmet used for basic security
 import volleyball from 'volleyball'; // Import volleyball for a logger for the server
 import db from './db'; // Import database file. This file creates the database connection (MongoDB using mLab)
 db; // This just makes sure the database file is run
+
 const app: express.Application = express(); // Create application
 const PORT: number | string = process.env.PORT || 3000; // Define port
 app.use(helmet()); // Use helmet
